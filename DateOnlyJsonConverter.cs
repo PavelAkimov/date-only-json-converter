00d65fb2aebd535a2d2993f101ff85ab386360f6
KilgarraDrago <paulne@yandex.ru>
2020-09-22 03:45:07 +0400

    Init repo

---
+++ DateOnlyJsonConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Newtonsoft.Json.Converters
+{
+    public class DateOnlyJsonConverter : DateTimeConverterBase
+    {
+        private string dateFormat;
+
+        public DateOnlyJsonConverter()
+        {
+            dateFormat = "yyyy-MM-dd";
+        }
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType != typeof(DateTime))
+            {
+                throw new InvalidCastException();
+            }
+            return Convert.ToDateTime(reader.Value);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (!(value is DateTime))
+            {
+                throw new InvalidCastException();
+            }
+            writer.WriteValue(((DateTime)value).ToString(dateFormat));
+            writer.Flush();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeTransferTestClass srcObject = new DateTimeTransferTestClass()
+            {
+                DateWithTime1 = DateTime.Now,
+                DateOnly = DateTime.Today,
+                DateWithTime2 = DateTime.Now.AddHours(-2)
+            };
+            Console.WriteLine("Before serialization:");
+            srcObject.Print();
+
+            JsonSerializerSettings microsoftDateFormatSettings = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+            };
+            var srcJSON = JsonConvert.SerializeObject(srcObject, microsoftDateFormatSettings);
+            Console.WriteLine("Serialized object:");
+            Console.WriteLine(srcJSON);
+
+            Console.WriteLine("After deserialization:");
+            DateTimeTransferTestClass dstObject = JsonConvert.DeserializeObject<DateTimeTransferTestClass>(srcJSON);
+            dstObject.Print();
+
+            Console.ReadLine();
+        }
+
+        
+        public class DateTimeTransferTestClass
+        {
+            public DateTime DateWithTime1 { get; set; }
+            [JsonConverter(typeof(DateOnlyJsonConverter))]
+            public DateTime DateOnly { get; set; }
+            public DateTime DateWithTime2 { get; set; }
+
+            internal void Print()
+            {
+                Console.WriteLine($"Date with tme 1: {DateWithTime1}");
+                Console.WriteLine($"Date only: {DateOnly}");
+                Console.WriteLine($"Date with tme 2: {DateWithTime2}");
+            }
+        }
+    }   
+}
